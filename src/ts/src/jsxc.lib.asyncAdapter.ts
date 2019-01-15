7b25897b632608dc5c318906d6cf1f707974bca0
anesdi73 <aaed@gmv.es>
2019-01-15 16:45:19 +0100

    add comments. Improve session id generation. Allows Chunksize definition

--- src/ts/src/jsxc.lib.asyncAdapter.ts
+++ src/ts/src/jsxc.lib.asyncAdapter.ts
@@ -1,6 +1,15 @@
 class AsyncAdapter {
-	static sendAsync(jid, file): Promise<string> {
-		const sid = 'sifileTransferSession:' + new Date();
+	/**
+     * Send Si File transfer negotiation asynchronusly (only IBB transport is valid)
+     *
+     * @static
+     * @param {string} jid full jid where the file is being sent
+     * @param {string} file file we are about to send
+     * @returns {Promise<string>} promise including the session id
+     * @memberof AsyncAdapter
+     */
+    static sendAsync(jid: string, file: File): Promise<string> {
+		const sid = 'siFileTransferSession:' + Date.now() + Math.random();
 		const promise = new Promise<string>((resolve, reject) => {
 			jsxc.xmpp.conn.si_filetransfer.send(jid, sid, file.name, file.size, file.type, err => {
                 if (err) {
@@ -13,20 +22,42 @@ class AsyncAdapter {
 		return promise;
 	}
 
-	static openAsync(to, sid): Promise<number> {
-		const blockSize = 4096;
+
+    /**
+     * Send IBB open message asynchronusly. It includes some metadata for the data we are about to send
+     *
+     * @static
+     * @param {string} to. full jid where the file is being sent
+     * @param {string} sid . Session id
+     * @param {number} [blockSize] maximum packet size in bytes
+     * @returns {Promise<number>} promise including the packet size
+     * @memberof AsyncAdapter
+     */
+    static openAsync(to: string, sid: string, blockSize?: number): Promise<number> {
+		const bs = blockSize || 4096;
 		const promise = new Promise<number>((resolve, reject) => {
-			jsxc.xmpp.conn.ibb.open(to, sid, blockSize, function(err) {
+			jsxc.xmpp.conn.ibb.open(to, sid, bs, function(err) {
 				if (err) {
 					reject(err);
 				} else {
-					resolve(blockSize);
+					resolve(bs);
 				}
 			});
 		});
 		return promise;
 	}
-	static dataAsync(to: string, sid: string, seq: number, data: string): Promise<void> {
+	/**
+     * Send IBB data message asynchronusly. It includes a chunk of data
+     *
+     * @static
+     * @param {string} to full jid where the file is being sent
+     * @param {string} sid  Session id
+     * @param {number} seq chunk sequence number we are sending
+     * @param {string} data base64 payload
+     * @returns {Promise<void>}
+     * @memberof AsyncAdapter
+     */
+    static dataAsync(to: string, sid: string, seq: number, data: string): Promise<void> {
 		const promise = new Promise<void>((resolve, reject) => {
 			jsxc.xmpp.conn.ibb.data(to, sid, seq, data, function(err) {
 				if (err) {
@@ -38,7 +69,17 @@ class AsyncAdapter {
 		});
 		return promise;
 	}
-	static closeAsync(to, sid): Promise<void> {
+
+    /**
+     * Send IBB close message asynchronusly. It reports transmission has finished
+     *
+     * @static
+     * @param {string} to full jid where the file is being sent
+     * @param {string} sid Session id
+     * @returns {Promise<void>}
+     * @memberof AsyncAdapter
+     */
+    static closeAsync(to: string, sid: string): Promise<void> {
 		const promise = new Promise<void>((resolve, reject) => {
 			jsxc.xmpp.conn.ibb.close(to, sid, function(err) {
 				if (err) {
